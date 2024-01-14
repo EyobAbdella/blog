cb7228d275c827dc70af58dc6124d63520329336
Eyob Abdella <eyobabdellasharo@gmail.com>
2024-01-14 15:31:31 +0300

    Implement user signup: form, views, URL, and templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from .forms import SignUpForm
 
-# Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = SignUpForm()
+    return render(request, "auth/signup.html", {"form": form})
+
+
+def home(request):
+    return render(request, "home.html")
