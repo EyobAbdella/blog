d387fd32b0f33822f450b3dd85a523c1b403aa49
Kalkidan <kalkidanteshome951@gmail.com>
2024-01-15 13:18:59 -0500

    Add login page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import login
 from django.shortcuts import redirect
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import SignUpForm
 
 
@@ -14,6 +15,18 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, "auth/signup.html", {"form": form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, "auth/login.html", {"form": form})
 
 
 def home(request):
