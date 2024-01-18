5f6990f3b66285719c97b796c5002c3dca23c7f2
Eyob Abdella <eyobabdellasharo@gmail.com>
2024-01-19 00:21:55 +0300

    Finish login page

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
 
 
@@ -9,3 +9,11 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("email", "first_name", "last_name", "password1", "password2")
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs["class"] = "form-content"
+        self.fields["password"].widget.attrs["class"] = "form-content"
--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import login
 from django.shortcuts import redirect
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import SignUpForm
+from .forms import CustomAuthenticationForm, SignUpForm
 
 
 def signup(request):
@@ -19,12 +19,12 @@ def signup(request):
 
 def user_login(request):
     if request.method == "POST":
-        form = AuthenticationForm(request, request.POST)
+        form = CustomAuthenticationForm(request, request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect("home")
     else:
-        form = AuthenticationForm()
+        form = CustomAuthenticationForm()
 
     return render(request, "auth/login.html", {"form": form})
