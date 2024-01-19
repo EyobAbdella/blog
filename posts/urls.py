c5bc99f266443f7d92016272f2f1066c50258706
Kalkidan <kalkidanteshome951@gmail.com>
2024-01-19 11:46:48 -0500

    Add blog detail page

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.BlogListView.as_view(), name="home"),
     path("create/", views.BlogCreateView.as_view(), name="create"),
+    path("detail/<slug>/", views.BlogDetailView.as_view(), name="detail"),
 ]
